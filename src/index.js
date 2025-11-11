bb306dbfefb5639ad02093f8e605ff9c0c7473e9
VicenteAsmussen <vicente.asmussen2201@alumnos.ubiobio.cl>
2025-11-11 19:48:51 -0300

    contenido del config y .env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import fs from 'fs';
 
 const app = express();
-const PORT = process.nextTick.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,11 +11,15 @@ app.use(express.json());
 //Ruta de prueba
 app.get('/api/evaluaciones', (req, res) => {
     try {
-        const dbRaw = fs.readFilesSync('db.json');
+        const dbRaw = fs.readFileSync('db.json');
         const db = JSON.parse(dbRaw);
         res.status(200).json(db.evaluaciones);
     }
     catch (error) {
+        console.error(error)
         res.status(500).json({ message: "Error al leer la base de datos" });
     }
 })
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
