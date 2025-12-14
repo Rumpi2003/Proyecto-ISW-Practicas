15e54d097c6620ccbdbf50f54e7280d28da8dda2
VicenteAsmussen <vicente.asmussen2201@alumnos.ubiobio.cl>
2025-12-14 04:21:19 -0300

    Delete src directory

--- src/routes/profile.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-// Aquí irán las rutas de perfil cuando las necesites
-// router.get("/:id", getProfile);
-// router.put("/:id", updateProfile);
-
-export default router;
