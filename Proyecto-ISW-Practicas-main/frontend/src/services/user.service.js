689081564cbe8f769d5122dceb6482344d69a0f4
cristina moran valenzuela <cristina.moran2201@alumnos.ubiobio.cl>
2025-12-26 17:54:02 -0300

    feat: implementar función deleteUser en user.service

--- Proyecto-ISW-Practicas-main/frontend/src/services/user.service.js
+++ Proyecto-ISW-Practicas-main/frontend/src/services/user.service.js
@@ -1,11 +1,61 @@
 import axios from './root.service.js';
 
-export async function getMyProfile() {
+// Crear Estudiante
+export const createEstudiante = async (data) => {
     try {
-        //ruta protegida
-        const response = await axios.get('/profile/private'); 
+        const response = await axios.post('/users/estudiantes', data);
         return response.data;
     } catch (error) {
-        return error.response?.data;
+        throw error.response?.data || error.message;
     }
-}
+};
+
+export const getUsers = async () => {
+    try {
+        const response = await axios.get('/users');
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+// Crear Supervisor
+export const createSupervisor = async (data) => {
+    try {
+        const response = await axios.post('/users/supervisores', data);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+// Crear Encargado
+export const createEncargado = async (data) => {
+    try {
+        const response = await axios.post('/users/encargados', data);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
+
+// Obtener perfil (necesario para HomeEstudiante)
+export const getMyProfile = async () => {
+    try {
+        const response = await axios.get('/users/me');
+        return response.data;
+    } catch (error) {
+        console.error("Error obteniendo perfil:", error);
+        return null;
+    }
+};
+
+// Eliminar al usuario supervisor
+export const deleteUser = async (id) => {
+    try {
+        const response = await axios.delete(`/users/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response?.data || error.message;
+    }
+};
