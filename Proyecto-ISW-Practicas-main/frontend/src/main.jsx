742bc3572bfb8044dff8d47117a8d454dfbcf2b3
cristina moran valenzuela <cristina.moran2201@alumnos.ubiobio.cl>
2025-12-28 17:52:42 -0300

    Eliminación carpeta extra

--- Proyecto-ISW-Practicas-main/frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from "react-router-dom";
-import { router } from './routes/AppRouter'; 
-import '@styles/styles.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-);
