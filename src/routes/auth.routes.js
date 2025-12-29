51817ffdd2e14c6a9b26cdd3ab93deca54ba714a
VicenteAsmussen <vicente.asmussen2201@alumnos.ubiobio.cl>
2025-12-28 23:12:04 -0300

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
-
-const router = Router();
-
-router.post("/login", login);
-
-export default router;
